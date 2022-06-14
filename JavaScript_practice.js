009071102f89201f308a57e4c9531016970950bd
Dmitry-Drobov <dmitry.drobov@gmail.com>
2022-06-14 12:19:09 +0300

    add 2 JS files practice

---
+++ JavaScript_practice.js
@@ -0,0 +1,35 @@
+let user_name = "Dmitry"
+// let name2 = Dmitry
+
+// console.log(user_name)
+
+let age = 39
+// console.log(age)
+
+// let person = user_name + " " + age
+// console.log(user_name + " ______" + age)
+
+let user_age = 30
+let work_period = 10
+let total_time = user_age * work_period
+console.log("total_time = " , total_time)
+
+let morning = false
+console.log("morning = ", morning, typeof(morning))
+
+let evening = true
+console.log("evening = ", evening, typeof(evening))
+
+let apples = 15
+let cherry = 5
+let compare = apples >= cherry
+console.log("Compare =", compare)
+
+if (compare){
+    console.log("Compare is", compare)
+} else if (apples == 15){
+    console.log ("apples ==", apples)
+} 
+  else {
+    console.log("Compare is", false)
+}
