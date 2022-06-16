5a9037833d91218a71f1733713a9b40acfb87e03
Dmitry Drobov <106947033+Dmitry-Drobov@users.noreply.github.com>
2022-06-16 22:49:15 +0300

    Update JS. HW_1_add2.js

--- JS. HW_1_add2.js
+++ JS. HW_1_add2.js
@@ -9,7 +9,7 @@ const checkAge = function(age){
        
 if (age < age_2){
     console.log("You don't have access cause your age is less than " + age_2 + ".")
-} else if (age >= age_2 & age < age_3){
+} else if (age >= age_2 && age < age_3){
     console.log("Welcome!")
 } else if (age > age_3){
     console.log("Keep calm and look Culture channel.")
