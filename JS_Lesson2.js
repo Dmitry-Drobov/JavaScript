4b60cec832e79386f41edd1c11b03393168938dd
Dmitry-Drobov <dmitry.drobov@gmail.com>
2022-06-17 13:51:43 +0300

    add JS lesson 2

---
+++ JS_Lesson2.js
@@ -0,0 +1,66 @@
+// console.log("hello")
+
+// console.log(true || false)
+// console.log(false || true)
+// console.log(false || true || false)
+// console.log(true && false)
+// console.log(true && false || true)
+// console.log(true && false || true && true)
+
+// if (false || true){
+//     console.log("1")
+// } else {
+//     console.log("2")
+// }
+// if (false || true && false){
+//     console.log("1")
+// } else {
+//     console.log("2")
+// }
+// if (false && true || false){
+//     console.log("1")
+// } else {
+//     console.log("2")
+// }
+
+let dog_show = true
+let age = 18
+let money = 2000
+let dog = true
+
+// if (age >= 18 && money >= 1000 && dog){
+//     console.log("Get ticket")
+// } else if (age < 18){
+//     console.log ("Early")
+// } else if (money < 1000){
+//     console.log("Keep calm and work hard!")
+// } else if (!dog){
+//     console.log("Get cat")
+// }
+
+// switch (dog_show){
+//     case age >= 18:
+//         console.log("Time to win")
+//         // break;
+//     case money >= 1000:
+//         console.log("Rich")
+//         break;
+// }
+count = 0
+// while (count < 10){
+//     // console.log(count, "Win!!!")
+//     if (count % 2 == 0){
+//         console.log(count, "even")
+//     }
+//     count ++
+// }
+
+for (let i = 0; i < 10; i++){
+    console.log(i, "Win2")
+}
+
+mm = ["Alla", "Natalya", "Milana", "Dmitry", "Anatoliy"]
+// console.log(mm.length)
+for (let i = 0; i < mm.length +1; i++){
+    console.log(i, mm[i], "win")
+}
